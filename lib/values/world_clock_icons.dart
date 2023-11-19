7b5dcca8c3318868b40f541fc4b8319a5789ac0e
Parth Baraiya <parthbaraiya19.pb@gmail.com>
2023-11-19 19:13:55 +0530

    ➕ Adds new brand icons.

--- lib/values/world_clock_icons.dart
+++ lib/values/world_clock_icons.dart
@@ -13,4 +13,16 @@ class WorldClockIcons {
       IconData(0xe904, fontFamily: _fontFamily);
   static const IconData chevron_up = IconData(0xe905, fontFamily: _fontFamily);
   static const IconData exchange = IconData(0xe906, fontFamily: _fontFamily);
+
+  // Use with color: #3ddc84
+  static const IconData android = IconData(0xe907, fontFamily: _fontFamily);
+  // Use with color: #999
+  static const IconData apple = IconData(0xe908, fontFamily: _fontFamily);
+  static const IconData github = IconData(0xe909, fontFamily: _fontFamily);
+  static const IconData ios = IconData(0xe90a, fontFamily: _fontFamily);
+  // Use with color:  #e95420
+  static const IconData ubuntu = IconData(0xe90b, fontFamily: _fontFamily);
+
+  // Use with color: #0078d6
+  static const IconData windows = IconData(0xe90c, fontFamily: _fontFamily);
 }
